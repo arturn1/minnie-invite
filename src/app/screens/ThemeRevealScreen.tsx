827ad6ec9d2f6a4a4a05fd9b9b87b62e658e2be2
Artur <31557902+arturn1@users.noreply.github.com>
2024-12-08 18:31:33 -0300

    Update ThemeRevealScreen.tsx

--- src/app/screens/ThemeRevealScreen.tsx
+++ src/app/screens/ThemeRevealScreen.tsx
@@ -78,7 +78,6 @@ const ThemeRevealScreen = ({ onNext }: { onNext: () => void }) => {
           <li>Toalhas</li>
           <li>Perfumes</li>
         </CollapsibleSection>
-     //   <Carousel images={carouselImages} />
         <Button onClick={handleConfirm}>Confirmar</Button>
       </Content>
     </Container>
